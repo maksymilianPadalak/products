74969406e651af4eb85d886eacaf7a598f6722ea
Maks Padalak <maksymilian.padalak@gmail.com>
2023-01-08 20:57:22 +0100

    Extend Product schema

---
+++ src/enums/enums.ts
@@ -0,0 +1,4 @@
+export enum ProductType {
+  'ALBUM' = 'ALBUM',
+  'MOVIE' = 'MOVIE',
+}
--- src/product/product.schema.ts
+++ src/product/product.schema.ts
@@ -1,20 +1,28 @@
-import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { ProductType } from '../enums/enums';
 
 export type ProductDocument = HydratedDocument<Product>;
 
 @Schema()
 export class Product {
-  @Prop({ required: true })
+  @Prop({
+    required: true,
+    unique: true,
+    sparse: true,
+    index: true,
+  })
   name: string;
 
-  @Prop(
-    raw({
-      title: { type: String, required: true },
-      album: { type: String, required: true },
-      genre: { type: String },
-    }),
-  )
+  @Prop({
+    type: { type: String, required: true, enum: ProductType },
+    title: {
+      type: String,
+      required: true,
+    },
+    album: { type: String, required: true },
+    genre: { type: String },
+  })
   details: Record<string, any>;
 }
 
